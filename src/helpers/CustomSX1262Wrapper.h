6d10fab4f3a81f9bc46c0bcc12add0c16752e392
Scott Powell <sqij@protonmail.com>
2025-01-21 22:35:20 +1100

    * commenting out CAD detect, for now

--- src/helpers/CustomSX1262Wrapper.h
+++ src/helpers/CustomSX1262Wrapper.h
@@ -9,9 +9,11 @@ public:
   bool isReceiving() override { 
     if (((CustomSX1262 *)_radio)->isReceiving()) return true;
 
+#if 0  //  has BUG :-(  commenting out for now
     idle();  // put sx126x into standby
     // do some basic CAD (blocks for ~12780 micros (on SF 10)!)
     if (((CustomSX1262 *)_radio)->scanChannel() == RADIOLIB_LORA_DETECTED) return true;
+#endif
     return false;
   }
   float getLastRSSI() const override { return ((CustomSX1262 *)_radio)->getRSSI(); }
