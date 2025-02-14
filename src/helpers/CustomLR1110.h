e16dad778c7a2033fb5f70ed429745e4c49432c4
Florent <florent@frizoncorrea.fr>
2025-02-14 09:54:06 +0100

    helper files for t1000e and LR1110

---
+++ src/helpers/CustomLR1110.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <RadioLib.h>
+
+#define LR1110_IRQ_HAS_PREAMBLE                     0b0000000100  //  4     4     valid LoRa header received
+#define LR1110_IRQ_HEADER_VALID                     0b0000010000  //  4     4     valid LoRa header received
+
+class CustomLR1110 : public LR1110 {
+  public:
+    CustomLR1110(Module *mod) : LR1110(mod) { }
+
+    bool isReceiving() {
+      uint16_t irq = getIrqStatus();
+      bool hasPreamble = ((irq & LR1110_IRQ_HEADER_VALID) && (irq & LR1110_IRQ_HAS_PREAMBLE));
+      return hasPreamble;
+    }
+};
---
+++ src/helpers/CustomLR1110Wrapper.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "CustomLR1110.h"
+#include "RadioLibWrappers.h"
+
+class CustomLR1110Wrapper : public RadioLibWrapper {
+public:
+  CustomLR1110Wrapper(CustomLR1110& radio, mesh::MainBoard& board) : RadioLibWrapper(radio, board) { }
+  bool isReceiving() override { 
+    if (((CustomLR1110 *)_radio)->isReceiving()) return true;
+
+    idle();  // put sx126x into standby
+    // do some basic CAD (blocks for ~12780 micros (on SF 10)!)
+    bool activity = (((CustomLR1110 *)_radio)->scanChannel() == RADIOLIB_LORA_DETECTED);
+    idle();
+
+    return activity;
+  }
+
+  void onSendFinished() override {
+    RadioLibWrapper::onSendFinished();
+    _radio->setPreambleLength(8); // overcomes weird issues with small and big pkts
+  }
+
+  float getLastRSSI() const override { return ((CustomLR1110 *)_radio)->getRSSI(); }
+  float getLastSNR() const override { return ((CustomLR1110 *)_radio)->getSNR(); }
+};
