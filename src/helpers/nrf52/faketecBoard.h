a57053207f98464edd0cc1bd786a4d8fd6385ef6
taco <taco@sly.nu>
2025-02-25 16:20:35 +1100

    Added defines for SX126X_RXEN, SX126X_TXEN. Updated build target to enable privkey import/export.

--- src/helpers/nrf52/faketecBoard.h
+++ src/helpers/nrf52/faketecBoard.h
@@ -3,20 +3,21 @@
 #include <MeshCore.h>
 #include <Arduino.h>
 
-#define P_LORA_NSS D13 
-#define P_LORA_DIO_1 D11
-#define P_LORA_RESET D10
-#define P_LORA_BUSY  D16
-#define P_LORA_MISO  D15
-#define P_LORA_SCLK  D12
-#define P_LORA_MOSI  D14
-#define SX126X_POWER_EN EXT_VCC
-
+#define P_LORA_NSS D13 //P1.13 45
+#define P_LORA_DIO_1 D11 //P0.10 10
+#define P_LORA_RESET D10 //P0.09 9
+#define P_LORA_BUSY  D16 //P0.29 29
+#define P_LORA_MISO  D15 //P0.02 2
+#define P_LORA_SCLK  D12 //P1.11 43
+#define P_LORA_MOSI  D14 //P1.15 47
+#define SX126X_POWER_EN EXT_VCC //P0.13 13
+#define SX126X_RXEN D2 //P0.17
+#define SX126X_TXEN RADIOLIB_NC
 #define SX126X_DIO2_AS_RF_SWITCH  true
-#define SX126X_DIO3_TCXO_VOLTAGE   1.8
+#define SX126X_DIO3_TCXO_VOLTAGE (1.8f)
 
 #define  PIN_VBAT_READ PIN_A2
-#define  ADC_MULTIPLIER   (1.815) // dependent on voltage divider resistors. TODO: more accurate battery tracking
+#define  ADC_MULTIPLIER   (1.815f) // dependent on voltage divider resistors. TODO: more accurate battery tracking
 
 class faketecBoard : public mesh::MainBoard {
 protected:
