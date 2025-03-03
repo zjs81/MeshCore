03293be892133a65ae3c01cc19c3d08fe6af2685
Florent de Lamotte <florent@frizoncorrea.fr>
2025-03-03 13:26:37 +0100

    new Heltec_v3_companion_radio_wifi target

---
+++ src/helpers/esp32/SerialWifiInterface.cpp
@@ -0,0 +1,98 @@
+#include "SerialWifiInterface.h"
+#include <WiFi.h>
+
+void SerialWifiInterface::begin(int port) {
+  // wifi setup is handled outside of this class, only starts the server
+  server.begin(port);
+}
+
+// ---------- public methods
+void SerialWifiInterface::enable() { 
+  if (_isEnabled) return;
+
+  _isEnabled = true;
+  clearBuffers();
+}
+
+void SerialWifiInterface::disable() {
+  _isEnabled = false;
+}
+
+size_t SerialWifiInterface::writeFrame(const uint8_t src[], size_t len) {
+  if (len > MAX_FRAME_SIZE) {
+    Serial.printf("writeFrame(), frame too big, len=%d\n", len);
+    return 0;
+  }
+
+  if (deviceConnected && len > 0) {
+    if (send_queue_len >= FRAME_QUEUE_SIZE) {
+      Serial.println("writeFrame(), send_queue is full!");
+      return 0;
+    }
+
+    send_queue[send_queue_len].len = len;  // add to send queue
+    memcpy(send_queue[send_queue_len].buf, src, len);
+    send_queue_len++;
+
+    return len;
+  }
+  return 0;
+}
+
+#define  SER_WRITE_MIN_INTERVAL   0
+
+bool SerialWifiInterface::isWriteBusy() const {
+  return millis() < _last_write + SER_WRITE_MIN_INTERVAL;   // still too soon to start another write?
+}
+
+size_t SerialWifiInterface::checkRecvFrame(uint8_t dest[]) {
+  if (isWriteBusy())
+    return 0;
+
+  if (!client) client = server.available();
+
+  if (client.connected()) {
+    if (!deviceConnected) {
+      Serial.println("Got connexion");
+      deviceConnected = true;
+    }
+  } else {
+    if (deviceConnected) {
+      deviceConnected = false;
+      Serial.println("Disconnected");
+    }
+  }
+
+  if (deviceConnected) {
+    if (send_queue_len > 0) {   // first, check send queue
+      
+      _last_write = millis();
+      int len = send_queue[0].len;
+
+      uint8_t pkt[3+len]; // use same header as serial interface so client can delimit frames
+      pkt[0] = '>';
+      pkt[1] = (len & 0xFF);  // LSB
+      pkt[2] = (len >> 8);    // MSB
+      memcpy(&pkt[3], send_queue[0].buf, send_queue[0].len);
+      client.write(pkt, 3 + len);
+      send_queue_len--;
+      for (int i = 0; i < send_queue_len; i++) {   // delete top item from queue
+        send_queue[i] = send_queue[i + 1];
+      }
+    } else {
+      int len = client.available();
+      if (len > 0) {
+        uint8_t buf[MAX_FRAME_SIZE + 4];
+        client.readBytes(buf, len);
+        memcpy(dest, buf+3, len-3); // remove header (don't even check ... problems are on the other dir)
+        return len-3;
+      }
+    }
+  }
+
+  return 0;
+}
+
+bool SerialWifiInterface::isConnected() const {
+  return deviceConnected;  //pServer != NULL && pServer->getConnectedCount() > 0;
+}
