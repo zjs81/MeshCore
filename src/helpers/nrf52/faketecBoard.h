1a21a08845b1d89476fe97152592eb79bf74b515
taco <taco@sly.nu>
2025-03-11 15:38:37 +1100

    updated to MIT licensed board variant

--- src/helpers/nrf52/faketecBoard.h
+++ src/helpers/nrf52/faketecBoard.h
@@ -4,20 +4,20 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-#define P_LORA_NSS D13 //P1.13 45
-#define P_LORA_DIO_1 D11 //P0.10 10
-#define P_LORA_RESET D10 //P0.09 9
-#define P_LORA_BUSY  D16 //P0.29 29
-#define P_LORA_MISO  D15 //P0.02 2
-#define P_LORA_SCLK  D12 //P1.11 43
-#define P_LORA_MOSI  D14 //P1.15 47
-#define SX126X_POWER_EN EXT_VCC //P0.13 13
-#define SX126X_RXEN D2 //P0.17
+#define P_LORA_NSS 13 //P1.13 45
+#define P_LORA_DIO_1 11 //P0.10 10
+#define P_LORA_RESET 10 //P0.09 9
+#define P_LORA_BUSY  16 //P0.29 29
+#define P_LORA_MISO  15 //P0.02 2
+#define P_LORA_SCLK  12 //P1.11 43
+#define P_LORA_MOSI  14 //P1.15 47
+#define SX126X_POWER_EN 21 //P0.13 13
+#define SX126X_RXEN 2 //P0.17
 #define SX126X_TXEN RADIOLIB_NC
 #define SX126X_DIO2_AS_RF_SWITCH  true
 #define SX126X_DIO3_TCXO_VOLTAGE (1.8f)
 
-#define  PIN_VBAT_READ PIN_A2
+#define  PIN_VBAT_READ 17
 #define  ADC_MULTIPLIER   (1.815f) // dependent on voltage divider resistors. TODO: more accurate battery tracking
 
 class faketecBoard : public mesh::MainBoard {
