6a093743d32a619381c47e7a89d7965079ffd1eb
Scott Powell <sqij@protonmail.com>
2025-03-19 19:32:07 +1100

    * ver bump to v1.4.0

--- examples/companion_radio/main.cpp
+++ examples/companion_radio/main.cpp
@@ -141,11 +141,11 @@ static uint32_t _atoi(const char* sp) {
 #define FIRMWARE_VER_CODE    3
 
 #ifndef FIRMWARE_BUILD_DATE
-  #define FIRMWARE_BUILD_DATE   "13 Mar 2025"
+  #define FIRMWARE_BUILD_DATE   "19 Mar 2025"
 #endif
 
 #ifndef FIRMWARE_VERSION
-  #define FIRMWARE_VERSION   "v1.3.0"
+  #define FIRMWARE_VERSION   "v1.4.0"
 #endif
 
 #define CMD_APP_START              1
--- examples/simple_repeater/main.cpp
+++ examples/simple_repeater/main.cpp
@@ -22,11 +22,11 @@
 /* ------------------------------ Config -------------------------------- */
 
 #ifndef FIRMWARE_BUILD_DATE
-  #define FIRMWARE_BUILD_DATE   "13 Mar 2025"
+  #define FIRMWARE_BUILD_DATE   "19 Mar 2025"
 #endif
 
 #ifndef FIRMWARE_VERSION
-  #define FIRMWARE_VERSION   "v1.3.0"
+  #define FIRMWARE_VERSION   "v1.4.0"
 #endif
 
 #ifndef LORA_FREQ
