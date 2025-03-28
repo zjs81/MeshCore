022b43ef3d8b769b92b2390fec29cb5eeec8a06a
Scott Powell <sqij@protonmail.com>
2025-03-28 22:20:42 +1100

    * isReceiving() now includes either preamble detect or header detect

--- src/helpers/CustomSX1262.h
+++ src/helpers/CustomSX1262.h
@@ -3,6 +3,7 @@
 #include <RadioLib.h>
 
 #define SX126X_IRQ_HEADER_VALID                     0b0000010000  //  4     4     valid LoRa header received
+#define SX126X_IRQ_PREAMBLE_DETECTED           0x04
 
 class CustomSX1262 : public SX1262 {
   public:
@@ -10,7 +11,7 @@ class CustomSX1262 : public SX1262 {
 
     bool isReceiving() {
       uint16_t irq = getIrqFlags();
-      bool hasPreamble = (irq & SX126X_IRQ_HEADER_VALID);
-      return hasPreamble;
+      bool detected = (irq & SX126X_IRQ_HEADER_VALID) || (irq & SX126X_IRQ_PREAMBLE_DETECTED);
+      return detected;
     }
 };
