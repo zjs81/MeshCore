6092f5737ce69b86c440e0c52e6237e61c9660da
cod3doomy <cwdunlop@proton.me>
2025-04-08 09:12:43 -0700

    Initial commit with t-beam supreme sx1262 additions

---
+++ variants/lilygo_tbeam_supreme_SX1262/target.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <RadioLib.h>
+#include <helpers/RadioLibWrappers.h>
+#include <helpers/TBeamS3SupremeBoard.h>
+#include <helpers/CustomSX1262Wrapper.h>
+#include <helpers/AutoDiscoverRTCClock.h>
+
+extern TBeamS3SupremeBoard board;
+extern WRAPPER_CLASS radio_driver;
+extern AutoDiscoverRTCClock rtc_clock;
+
+bool radio_init();
+uint32_t radio_get_rng_seed();
+void radio_set_params(float freq, float bw, uint8_t sf, uint8_t cr);
+void radio_set_tx_power(uint8_t dbm);
+mesh::LocalIdentity radio_new_identity();
