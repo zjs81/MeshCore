d072e7b57560f1549b94b99c0f0acd46fe620b50
Scott Powell <sqij@protonmail.com>
2025-05-09 18:12:42 +1000

    * ver bump to v1.6.0

--- examples/simple_room_server/main.cpp
+++ examples/simple_room_server/main.cpp
@@ -22,11 +22,11 @@
 /* ------------------------------ Config -------------------------------- */
 
 #ifndef FIRMWARE_BUILD_DATE
-  #define FIRMWARE_BUILD_DATE   "21 Apr 2025"
+  #define FIRMWARE_BUILD_DATE   "9 May 2025"
 #endif
 
 #ifndef FIRMWARE_VERSION
-  #define FIRMWARE_VERSION   "v1.5.1"
+  #define FIRMWARE_VERSION   "v1.6.0"
 #endif
 
 #ifndef LORA_FREQ
