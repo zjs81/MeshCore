5d9e7b4567b216f9a6f4c0926ce5c1b000d5b20e
Normunds Gavars <ngavars@proton.me>
2025-05-19 20:30:58 +0300

    Remove unnecessary include

--- variants/promicro/target.h
+++ variants/promicro/target.h
@@ -7,17 +7,16 @@
 #include <helpers/CustomSX1262Wrapper.h>
 #include <helpers/CustomLLCC68Wrapper.h>
 #include <helpers/AutoDiscoverRTCClock.h>
-#include <helpers/SensorManager.h>
 #ifdef DISPLAY_CLASS
   #include <helpers/ui/SSD1306Display.h>
 #endif
 
-#define NUM_SENSOR_SETTINGS 3
 #include <helpers/sensors/EnvironmentSensorManager.h>
 
 extern PromicroBoard board;
 extern WRAPPER_CLASS radio_driver;
 extern AutoDiscoverRTCClock rtc_clock;
+extern EnvironmentSensorManager sensors;
 
 #ifdef DISPLAY_CLASS
   extern DISPLAY_CLASS display;
@@ -29,4 +28,3 @@ void radio_set_params(float freq, float bw, uint8_t sf, uint8_t cr);
 void radio_set_tx_power(uint8_t dbm);
 mesh::LocalIdentity radio_new_identity();
 
-extern EnvironmentSensorManager sensors;
