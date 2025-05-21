98d94d9423066518c9f61aff7a0564b1651ef842
Normunds Gavars <ngavars@proton.me>
2025-05-22 00:19:00 +0300

    Remove sensor wrapper classes and simplify. Switch to Adafruit libs for sensors.

--- src/helpers/sensors/EnvironmentSensorManager.cpp
+++ src/helpers/sensors/EnvironmentSensorManager.cpp
@@ -1,10 +1,49 @@
 #include "EnvironmentSensorManager.h"
 
+static Adafruit_AHTX0 AHTX0;
+static Adafruit_BME280 BME280;
+static Adafruit_INA3221 INA3221;
+static Adafruit_INA219 INA219(TELEM_INA219_ADDRESS);
+
 bool EnvironmentSensorManager::begin() {
-  INA3221_sensor.begin();
-  INA219_sensor.begin();
-  AHTX0_sensor.begin();
-  BME280_sensor.begin();
+
+  if (INA3221.begin(TELEM_INA3221_ADDRESS, &Wire)) {
+      MESH_DEBUG_PRINTLN("Found INA3221 at address: %02X", TELEM_INA3221_ADDRESS);
+      MESH_DEBUG_PRINTLN("%04X %04X", INA3221.getDieID(), INA3221.getManufacturerID());
+
+      for(int i = 0; i < 3; i++) {
+        INA3221.setShuntResistance(i, TELEM_INA3221_SHUNT_VALUE);
+      }
+      INA3221_initialized = true;
+  } else {
+      INA3221_initialized = false;
+      MESH_DEBUG_PRINTLN("INA3221 was not found at I2C address %02X", TELEM_INA3221_ADDRESS);
+  }
+
+  if (INA219.begin(&Wire)) {
+      MESH_DEBUG_PRINTLN("Found INA219 at address: %02X", TELEM_INA219_ADDRESS);
+      INA219_initialized = true;
+  } else {
+      INA219_initialized = false;
+      MESH_DEBUG_PRINTLN("INA219 was not found at I2C address %02X", TELEM_INA219_ADDRESS);
+  }
+
+  if (AHTX0.begin(&Wire, 0, TELEM_AHTX_ADDRESS)) {
+      MESH_DEBUG_PRINTLN("Found AHT10/AHT20 at address: %02X", TELEM_AHTX_ADDRESS);
+      AHTX0_initialized = true;
+  } else {
+      AHTX0_initialized = false;
+      MESH_DEBUG_PRINTLN("AHT10/AHT20 was not found at I2C address %02X", TELEM_AHTX_ADDRESS);
+  }
+
+  if (BME280.begin(TELEM_BME280_ADDRESS, &Wire)) {
+      MESH_DEBUG_PRINTLN("Found BME280 at address: %02X", TELEM_BME280_ADDRESS);
+      MESH_DEBUG_PRINTLN("BME sensor ID: %02X", BME280.sensorID);
+      BME280_initialized = true;
+  } else {
+      BME280_initialized = false;
+      MESH_DEBUG_PRINTLN("BME280 was not found at I2C address %02X", TELEM_BME280_ADDRESS);
+  }
 
   return true;
 }
@@ -12,63 +51,40 @@ bool EnvironmentSensorManager::begin() {
 bool EnvironmentSensorManager::querySensors(uint8_t requester_permissions, CayenneLPP& telemetry) {
   next_available_channel = TELEM_CHANNEL_SELF + 1;
   if (requester_permissions & TELEM_PERM_ENVIRONMENT) {
-    if (INA3221_sensor.isInitialized()) {
-      for(int i = 0; i < 3; i++) {
+    if (INA3221_initialized) {
+      for(int i = 0; i < TELEM_INA3221_NUM_CHANNELS; i++) {
         // add only enabled INA3221 channels to telemetry
-        if (INA3221_sensor.getChannelEnabled(i)) {
-          telemetry.addVoltage(next_available_channel, INA3221_sensor.getVoltage(i));
-          telemetry.addCurrent(next_available_channel, INA3221_sensor.getCurrent(i));
-          telemetry.addPower(next_available_channel, INA3221_sensor.getPower(i));
+        if (INA3221.isChannelEnabled(i)) {
+          float voltage = INA3221.getBusVoltage(i);
+          float current = INA3221.getCurrentAmps(i);
+          telemetry.addVoltage(next_available_channel, voltage);
+          telemetry.addCurrent(next_available_channel, current);
+          telemetry.addPower(next_available_channel, voltage * current);
           next_available_channel++;
         }
       }
     }
-    if (INA219_sensor.isInitialized()) {
-        telemetry.addVoltage(next_available_channel, INA219_sensor.getVoltage());
-        telemetry.addCurrent(next_available_channel, INA219_sensor.getCurrent());
-        telemetry.addPower(next_available_channel, INA219_sensor.getPower());
+    if (INA219_initialized) {
+        telemetry.addVoltage(next_available_channel, INA219.getBusVoltage_V());
+        telemetry.addCurrent(next_available_channel, INA219.getCurrent_mA() / 1000);
+        telemetry.addPower(next_available_channel, INA219.getPower_mW() / 1000);
         next_available_channel++;
     }
-    if (AHTX0_sensor.isInitialized()) {
-        telemetry.addTemperature(TELEM_CHANNEL_SELF, AHTX0_sensor.getTemperature());
-        telemetry.addRelativeHumidity(TELEM_CHANNEL_SELF, AHTX0_sensor.getRelativeHumidity());
+    if (AHTX0_initialized) {
+      sensors_event_t humidity, temp;
+      AHTX0.getEvent(&humidity, &temp);
+
+      telemetry.addTemperature(TELEM_CHANNEL_SELF, temp.temperature);
+      telemetry.addRelativeHumidity(TELEM_CHANNEL_SELF, humidity.relative_humidity);      
     }
-    if (BME280_sensor.isInitialized()) {
-        telemetry.addTemperature(TELEM_CHANNEL_SELF, BME280_sensor.getTemperature());
-        telemetry.addRelativeHumidity(TELEM_CHANNEL_SELF, BME280_sensor.getRelativeHumidity());
-        telemetry.addBarometricPressure(TELEM_CHANNEL_SELF, BME280_sensor.getBarometricPressure());
-        telemetry.addAltitude(TELEM_CHANNEL_SELF, BME280_sensor.getAltitude());
+
+    if (BME280_initialized) {
+        telemetry.addTemperature(TELEM_CHANNEL_SELF, BME280.readTemperature());
+        telemetry.addRelativeHumidity(TELEM_CHANNEL_SELF, BME280.readHumidity());
+        telemetry.addBarometricPressure(TELEM_CHANNEL_SELF, BME280.readPressure());
+        telemetry.addAltitude(TELEM_CHANNEL_SELF, BME280.readAltitude(TELEM_BME280_SEALEVELPRESSURE_HPA));
     }
   }
 
   return true;
-}
-
-int EnvironmentSensorManager::getNumSettings() const {
-  return NUM_SENSOR_SETTINGS;
-}
-
-const char* EnvironmentSensorManager::getSettingName(int i) const {
-  if (i >= 0 && i < NUM_SENSOR_SETTINGS) {
-    return INA3221_CHANNEL_NAMES[i];
-  }
-  return NULL;
-}
-
-const char* EnvironmentSensorManager::getSettingValue(int i) const {
-  if (i >= 0 && i < NUM_SENSOR_SETTINGS) {
-    return INA3221_sensor.getChannelEnabled(i) == true ? "1" : "0";
-  }
-  return NULL;
-}
-
-bool EnvironmentSensorManager::setSettingValue(const char* name, const char* value) {
-  for (int i = 0; i < NUM_SENSOR_SETTINGS; i++) {
-    if (strcmp(name, INA3221_CHANNEL_NAMES[i]) == 0) {
-        bool channel_enabled = strcmp(value, "1") == 0 ? true : false;
-        INA3221_sensor.setChannelEnabled(i, channel_enabled);
-        return true;
-    }
-  }
-  return false;
 }
--- src/helpers/sensors/EnvironmentSensorManager.h
+++ src/helpers/sensors/EnvironmentSensorManager.h
@@ -1,33 +1,37 @@
 #pragma once
 
 #include <helpers/SensorManager.h>
-#include "INA3221Sensor.h"
-#include "INA219Sensor.h"
-#include "AHTX0Sensor.h"
-#include "BME280Sensor.h"
+#include <Mesh.h>
+#include <Adafruit_INA3221.h>
+#include <Adafruit_INA219.h>
+#include <Adafruit_AHTX0.h>
+#include <Adafruit_BME280.h>
 
 #define NUM_SENSOR_SETTINGS 3
 
-#define TELEM_INA3221_SETTING_CH1 "INA3221-1"
-#define TELEM_INA3221_SETTING_CH2 "INA3221-2"
-#define TELEM_INA3221_SETTING_CH3 "INA3221-3"
+#define TELEM_AHTX_ADDRESS      0x38      // AHT10, AHT20 temperature and humidity sensor I2C address
+#define TELEM_BME280_ADDRESS    0x76      // BME280 environmental sensor I2C address
+#define TELEM_INA3221_ADDRESS   0x42      // INA3221 3 channel current sensor I2C address
+#define TELEM_INA219_ADDRESS    0x40      // INA219 single channel current sensor I2C address
+
+#define TELEM_INA3221_SHUNT_VALUE 0.100 // most variants will have a 0.1 ohm shunts
+#define TELEM_INA3221_NUM_CHANNELS 3
+
+#define TELEM_INA219_SHUNT_VALUE 0.100  // shunt value in ohms (may differ between manufacturers)
+#define TELEM_INA219_MAX_CURRENT 5
+
+#define TELEM_BME280_SEALEVELPRESSURE_HPA (1013.25)    // Athmospheric pressure at sea level
 
 class EnvironmentSensorManager : public SensorManager {
-// INA3221 channels in telemetry
-const char * INA3221_CHANNEL_NAMES[NUM_SENSOR_SETTINGS] = { TELEM_INA3221_SETTING_CH1, TELEM_INA3221_SETTING_CH2, TELEM_INA3221_SETTING_CH3};
-
 protected:
   int next_available_channel = TELEM_CHANNEL_SELF + 1;
-  INA3221Sensor INA3221_sensor;
-  AHTX0Sensor AHTX0_sensor;
-  INA219Sensor INA219_sensor;
-  BME280Sensor BME280_sensor;
+
+  bool INA3221_initialized = false;
+  bool INA219_initialized = false;
+  bool BME280_initialized = false;
+  bool AHTX0_initialized = false;
 public:
   EnvironmentSensorManager(){};
   bool begin() override;
   bool querySensors(uint8_t requester_permissions, CayenneLPP& telemetry) override;
-  int getNumSettings() const override;
-  const char* getSettingName(int i) const override;
-  const char* getSettingValue(int i) const override;
-  bool setSettingValue(const char* name, const char* value) override;
 };
