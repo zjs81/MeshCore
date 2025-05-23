400c4353dc72141140906c9cfcdf06123c093994
taco <taco@sly.nu>
2025-05-23 17:08:23 +1000

    REFACTOR: sensors are now wrapped in conditionals

--- src/helpers/sensors/EnvironmentSensorManager.cpp
+++ src/helpers/sensors/EnvironmentSensorManager.cpp
@@ -1,15 +1,45 @@
 #include "EnvironmentSensorManager.h"
 
-
-#include <helpers/sensors/LocationProvider.h>
-
+#if ENV_INCLUDE_AHTX0
 static Adafruit_AHTX0 AHTX0;
+#endif
+#if ENV_INCLUDE_BME280
 static Adafruit_BME280 BME280;
+#endif
+#if ENV_INCLUDE_INA3221
 static Adafruit_INA3221 INA3221;
+#endif
+#if ENV_INCLUDE_INA219
 static Adafruit_INA219 INA219(TELEM_INA219_ADDRESS);
+#endif
 
 bool EnvironmentSensorManager::begin() {
+  #if ENV_INCLUDE_GPS
+  initBasicGPS();
+  #endif
 
+  #if ENV_INCLUDE_AHTX0
+  if (AHTX0.begin(&Wire, 0, TELEM_AHTX_ADDRESS)) {
+    MESH_DEBUG_PRINTLN("Found AHT10/AHT20 at address: %02X", TELEM_AHTX_ADDRESS);
+    AHTX0_initialized = true;
+  } else {
+    AHTX0_initialized = false;
+    MESH_DEBUG_PRINTLN("AHT10/AHT20 was not found at I2C address %02X", TELEM_AHTX_ADDRESS);
+  }
+  #endif
+
+  #if ENV_INCLUDE_BME280
+  if (BME280.begin(TELEM_BME280_ADDRESS, &Wire)) {
+    MESH_DEBUG_PRINTLN("Found BME280 at address: %02X", TELEM_BME280_ADDRESS);
+    MESH_DEBUG_PRINTLN("BME sensor ID: %02X", BME280.sensorID());
+    BME280_initialized = true;
+  } else {
+    BME280_initialized = false;
+    MESH_DEBUG_PRINTLN("BME280 was not found at I2C address %02X", TELEM_BME280_ADDRESS);
+  }
+  #endif
+
+  #if ENV_INCLUDE_INA3221
   if (INA3221.begin(TELEM_INA3221_ADDRESS, &Wire)) {
       MESH_DEBUG_PRINTLN("Found INA3221 at address: %02X", TELEM_INA3221_ADDRESS);
       MESH_DEBUG_PRINTLN("%04X %04X", INA3221.getDieID(), INA3221.getManufacturerID());
@@ -22,7 +52,9 @@ bool EnvironmentSensorManager::begin() {
       INA3221_initialized = false;
       MESH_DEBUG_PRINTLN("INA3221 was not found at I2C address %02X", TELEM_INA3221_ADDRESS);
   }
+  #endif
 
+  #if ENV_INCLUDE_INA219
   if (INA219.begin(&Wire)) {
       MESH_DEBUG_PRINTLN("Found INA219 at address: %02X", TELEM_INA219_ADDRESS);
       INA219_initialized = true;
@@ -30,33 +62,39 @@ bool EnvironmentSensorManager::begin() {
       INA219_initialized = false;
       MESH_DEBUG_PRINTLN("INA219 was not found at I2C address %02X", TELEM_INA219_ADDRESS);
   }
+  #endif
 
-  if (AHTX0.begin(&Wire, 0, TELEM_AHTX_ADDRESS)) {
-      MESH_DEBUG_PRINTLN("Found AHT10/AHT20 at address: %02X", TELEM_AHTX_ADDRESS);
-      AHTX0_initialized = true;
-  } else {
-      AHTX0_initialized = false;
-      MESH_DEBUG_PRINTLN("AHT10/AHT20 was not found at I2C address %02X", TELEM_AHTX_ADDRESS);
-  }
-
-  if (BME280.begin(TELEM_BME280_ADDRESS, &Wire)) {
-      MESH_DEBUG_PRINTLN("Found BME280 at address: %02X", TELEM_BME280_ADDRESS);
-      MESH_DEBUG_PRINTLN("BME sensor ID: %02X", BME280.sensorID);
-      BME280_initialized = true;
-  } else {
-      BME280_initialized = false;
-      MESH_DEBUG_PRINTLN("BME280 was not found at I2C address %02X", TELEM_BME280_ADDRESS);
-  }
-  initSerialGPS();
   return true;
 }
 
 bool EnvironmentSensorManager::querySensors(uint8_t requester_permissions, CayenneLPP& telemetry) {
-  if (requester_permissions & TELEM_PERM_LOCATION) {   // does requester have permission?
-    telemetry.addGPS(TELEM_CHANNEL_SELF, node_lat, node_lon, 0.0f);
-  }
   next_available_channel = TELEM_CHANNEL_SELF + 1;
+
+  if (requester_permissions & TELEM_PERM_LOCATION) {
+    telemetry.addGPS(TELEM_CHANNEL_SELF, node_lat, node_lon, 0.0f); // allow lat/lon via telemetry even if no GPS is detected
+  }
+
   if (requester_permissions & TELEM_PERM_ENVIRONMENT) {
+
+    #if ENV_INCLUDE_AHTX0
+    if (AHTX0_initialized) {
+      sensors_event_t humidity, temp;
+      AHTX0.getEvent(&humidity, &temp);
+      telemetry.addTemperature(TELEM_CHANNEL_SELF, temp.temperature);
+      telemetry.addRelativeHumidity(TELEM_CHANNEL_SELF, humidity.relative_humidity);      
+    }
+    #endif
+
+    #if ENV_INCLUDE_BME280
+    if (BME280_initialized) {
+      telemetry.addTemperature(TELEM_CHANNEL_SELF, BME280.readTemperature());
+      telemetry.addRelativeHumidity(TELEM_CHANNEL_SELF, BME280.readHumidity());
+      telemetry.addBarometricPressure(TELEM_CHANNEL_SELF, BME280.readPressure());
+      telemetry.addAltitude(TELEM_CHANNEL_SELF, BME280.readAltitude(TELEM_BME280_SEALEVELPRESSURE_HPA));
+    }
+    #endif
+
+    #if ENV_INCLUDE_INA3221
     if (INA3221_initialized) {
       for(int i = 0; i < TELEM_INA3221_NUM_CHANNELS; i++) {
         // add only enabled INA3221 channels to telemetry
@@ -70,49 +108,46 @@ bool EnvironmentSensorManager::querySensors(uint8_t requester_permissions, Cayen
         }
       }
     }
+    #endif
+
+    #if ENV_INCLUDE_INA219
     if (INA219_initialized) {
         telemetry.addVoltage(next_available_channel, INA219.getBusVoltage_V());
         telemetry.addCurrent(next_available_channel, INA219.getCurrent_mA() / 1000);
         telemetry.addPower(next_available_channel, INA219.getPower_mW() / 1000);
         next_available_channel++;
     }
-    if (AHTX0_initialized) {
-      sensors_event_t humidity, temp;
-      AHTX0.getEvent(&humidity, &temp);
+    #endif
 
-      telemetry.addTemperature(TELEM_CHANNEL_SELF, temp.temperature);
-      telemetry.addRelativeHumidity(TELEM_CHANNEL_SELF, humidity.relative_humidity);      
-    }
-
-    if (BME280_initialized) {
-        telemetry.addTemperature(TELEM_CHANNEL_SELF, BME280.readTemperature());
-        telemetry.addRelativeHumidity(TELEM_CHANNEL_SELF, BME280.readHumidity());
-        telemetry.addBarometricPressure(TELEM_CHANNEL_SELF, BME280.readPressure());
-        telemetry.addAltitude(TELEM_CHANNEL_SELF, BME280.readAltitude(TELEM_BME280_SEALEVELPRESSURE_HPA));
-    }
   }
-
 
   return true;
 }
 
 
 int EnvironmentSensorManager::getNumSettings() const {
+  #if ENV_INCLUDE_GPS
   return gps_detected ? 1 : 0;  // only show GPS setting if GPS is detected
+  #endif
 }
 
 const char* EnvironmentSensorManager::getSettingName(int i) const {
+  #if ENV_INCLUDE_GPS
   return (gps_detected && i == 0) ? "gps" : NULL;
+  #endif
 }
 
 const char* EnvironmentSensorManager::getSettingValue(int i) const {
+  #if ENV_INCLUDE_GPS
   if (gps_detected && i == 0) {
     return gps_active ? "1" : "0";
   }
+  #endif
   return NULL;
 }
 
 bool EnvironmentSensorManager::setSettingValue(const char* name, const char* value) {
+  #if ENV_INCLUDE_GPS
   if (gps_detected && strcmp(name, "gps") == 0) {
     if (strcmp(value, "0") == 0) {
       stop_gps();
@@ -121,13 +156,12 @@ bool EnvironmentSensorManager::setSettingValue(const char* name, const char* val
     }
     return true;
   }
+  #endif
   return false;  // not supported
 }
 
-
-
-
-void EnvironmentSensorManager::initSerialGPS() {
+#if ENV_INCLUDE_GPS
+void EnvironmentSensorManager::initBasicGPS() {
   Serial1.setPins(PIN_GPS_TX, PIN_GPS_RX);
   Serial1.begin(9600);
 
@@ -178,3 +212,4 @@ void EnvironmentSensorManager::loop() {
     next_gps_update = millis() + 1000;
   }
 }
+#endif
--- src/helpers/sensors/EnvironmentSensorManager.h
+++ src/helpers/sensors/EnvironmentSensorManager.h
@@ -1,45 +1,59 @@
 #pragma once
 
+#include <Mesh.h>
 #include <helpers/SensorManager.h>
 #include <helpers/sensors/LocationProvider.h>
-#include <Mesh.h>
-#include <Adafruit_INA3221.h>
-#include <Adafruit_INA219.h>
-#include <Adafruit_AHTX0.h>
-#include <Adafruit_BME280.h>
 
+
+#if ENV_INCLUDE_AHTX0
 #define TELEM_AHTX_ADDRESS      0x38      // AHT10, AHT20 temperature and humidity sensor I2C address
+#include <Adafruit_AHTX0.h>
+#endif
+#if ENV_INCLUDE_BME280
 #define TELEM_BME280_ADDRESS    0x76      // BME280 environmental sensor I2C address
+#define TELEM_BME280_SEALEVELPRESSURE_HPA (1013.25)    // Athmospheric pressure at sea level
+#include <Adafruit_BME280.h>
+#endif
+#if ENV_INCLUDE_INA3221
 #define TELEM_INA3221_ADDRESS   0x42      // INA3221 3 channel current sensor I2C address
-#define TELEM_INA219_ADDRESS    0x40      // INA219 single channel current sensor I2C address
-
 #define TELEM_INA3221_SHUNT_VALUE 0.100 // most variants will have a 0.1 ohm shunts
 #define TELEM_INA3221_NUM_CHANNELS 3
+#include <Adafruit_INA3221.h>
+#endif
+#if ENV_INCLUDE_INA219
+#define TELEM_INA219_ADDRESS    0x40      // INA219 single channel current sensor I2C address
+#include <Adafruit_INA219.h>
+#endif
 
-#define TELEM_BME280_SEALEVELPRESSURE_HPA (1013.25)    // Athmospheric pressure at sea level
+
 
 class EnvironmentSensorManager : public SensorManager {
 protected:
   int next_available_channel = TELEM_CHANNEL_SELF + 1;
 
+  bool AHTX0_initialized = false;
+  bool BME280_initialized = false;
   bool INA3221_initialized = false;
   bool INA219_initialized = false;
-  bool BME280_initialized = false;
-  bool AHTX0_initialized = false;
   
-  bool gps_active = false;
-  bool gps_detected = false;
   LocationProvider* _location;
+  bool gps_detected = false;
+  bool gps_active = false;
 
+  #if ENV_INCLUDE_GPS
   void start_gps();
   void stop_gps();
-  void initSerialGPS();
+  void initBasicGPS();
+  #endif
+
 
 public:
   EnvironmentSensorManager(LocationProvider &location): _location(&location){};
   bool begin() override;
   bool querySensors(uint8_t requester_permissions, CayenneLPP& telemetry) override;  
+  #if ENV_INCLUDE_GPS
   void loop() override;
+  #endif
   int getNumSettings() const override;
   const char* getSettingName(int i) const override;
   const char* getSettingValue(int i) const override;
