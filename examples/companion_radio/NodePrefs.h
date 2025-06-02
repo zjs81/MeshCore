9fe218e0d8e67322c1377b380cfee95fa7ec0a3c
hank <hank@jointsecurityarea.org>
2025-06-01 20:41:04 -0700

    Reverting format changes to NodePrefs
    But changing to pragma once.

--- examples/companion_radio/NodePrefs.h
+++ examples/companion_radio/NodePrefs.h
@@ -1,11 +1,11 @@
 #pragma once
 #include <cstdint> // For uint8_t, uint32_t
 
-#define TELEM_MODE_DENY        0
-#define TELEM_MODE_ALLOW_FLAGS 1 // use contact.flags
-#define TELEM_MODE_ALLOW_ALL   2
+#define TELEM_MODE_DENY            0
+#define TELEM_MODE_ALLOW_FLAGS     1     // use contact.flags
+#define TELEM_MODE_ALLOW_ALL       2
 
-struct NodePrefs { // persisted to file
+struct NodePrefs {  // persisted to file
   float airtime_factor;
   char node_name[32];
   float freq;
