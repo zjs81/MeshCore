365cb8963444a40021f966a4e0330e6e342e9728
Scott Powell <sqij@protonmail.com>
2025-07-24 20:30:35 +1000

    * ver bump to 1.7.4

--- examples/simple_repeater/main.cpp
+++ examples/simple_repeater/main.cpp
@@ -22,11 +22,11 @@
 /* ------------------------------ Config -------------------------------- */
 
 #ifndef FIRMWARE_BUILD_DATE
-  #define FIRMWARE_BUILD_DATE   "15 Jul 2025"
+  #define FIRMWARE_BUILD_DATE   "24 Jul 2025"
 #endif
 
 #ifndef FIRMWARE_VERSION
-  #define FIRMWARE_VERSION   "v1.7.3"
+  #define FIRMWARE_VERSION   "v1.7.4"
 #endif
 
 #ifndef LORA_FREQ
