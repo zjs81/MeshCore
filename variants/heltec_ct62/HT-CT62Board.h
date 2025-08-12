86ec82fd06768a8d4afc5a35edb7b12af3640d36
Scott Powell <sqij@protonmail.com>
2025-08-12 20:56:59 +1000

    * Heltec CT62: sensor role (with 2 channel relay support + 1 digital input)

--- variants/heltec_ct62/HT-CT62Board.h
+++ variants/heltec_ct62/HT-CT62Board.h
@@ -8,9 +8,35 @@
 #include <helpers/ESP32Board.h>
 
 class Heltec_CT62_Board : public ESP32Board {
-public:
+  uint32_t gpio_state = 0;
 
-uint16_t getBattMilliVolts() override {
+public:
+  void begin() {
+    ESP32Board::begin();
+#if defined(PIN_BOARD_RELAY_CH1) && defined(PIN_BOARD_RELAY_CH2) 
+    pinMode(PIN_BOARD_RELAY_CH1, OUTPUT);
+    pinMode(PIN_BOARD_RELAY_CH2, OUTPUT);
+#endif
+#if defined(PIN_BOARD_DIGITAL_IN)
+    pinMode(PIN_BOARD_DIGITAL_IN, INPUT);
+#endif
+  }
+  uint32_t getGpio() override {
+#if defined(PIN_BOARD_DIGITAL_IN)
+    return gpio_state | (digitalRead(PIN_BOARD_DIGITAL_IN) ? 1 : 0);
+#else
+    return 0;
+#endif
+  }
+  void setGpio(uint32_t values) override {
+#if defined(PIN_BOARD_RELAY_CH1) && defined(PIN_BOARD_RELAY_CH2) 
+    gpio_state = values;
+    digitalWrite(PIN_BOARD_RELAY_CH1, values & 2);
+    digitalWrite(PIN_BOARD_RELAY_CH2, values & 4);
+#endif
+  }
+
+  uint16_t getBattMilliVolts() override {
   #ifdef PIN_VBAT_READ
     analogReadResolution(12);         // ESP32-C3 ADC is 12-bit - 3.3/4096 (ref voltage/max counts)
     uint32_t raw = 0;
