afbfc6c6edd223a9f7bfd4eae0305394afa026b6
taco <taco@sly.nu>
2025-09-03 15:48:50 +1000

    add new keycodes

--- src/helpers/ui/UIScreen.h
+++ src/helpers/ui/UIScreen.h
@@ -2,13 +2,17 @@
 
 #include "DisplayDriver.h"
 
-#define KEY_LEFT     0xB4
-#define KEY_UP       0xB5
-#define KEY_DOWN     0xB6
-#define KEY_RIGHT    0xB7
-#define KEY_SELECT     10
-#define KEY_ENTER      13
-#define KEY_BACK       27   // Esc
+#define KEY_LEFT           0xB4
+#define KEY_UP             0xB5
+#define KEY_DOWN           0xB6
+#define KEY_RIGHT          0xB7
+#define KEY_SELECT           10
+#define KEY_ENTER            13
+#define KEY_CANCEL           27   // Esc
+#define KEY_HOME           0xF0
+#define KEY_NEXT           0xF1
+#define KEY_PREV           0xF2
+#define KEY_CONTEXT_MENU   0xF3
 
 class UIScreen {
 protected:
