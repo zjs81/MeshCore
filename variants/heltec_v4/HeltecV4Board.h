669bea04a0ff9e2fd9312809f3d0c205f2806212
Quency-D <hj_zzns@163.com>
2025-09-22 19:58:27 +0800

    add  heltec_v4 board.

---
+++ variants/heltec_v4/HeltecV4Board.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <Arduino.h>
+#include <helpers/RefCountedDigitalPin.h>
+#include <helpers/ESP32Board.h>
+#include <driver/rtc_io.h>
+
+class HeltecV4Board : public ESP32Board {
+
+public:
+  RefCountedDigitalPin periph_power;
+
+  HeltecV4Board() : periph_power(PIN_VEXT_EN,PIN_VEXT_EN_ACTIVE) { }
+
+  void begin();
+  void onBeforeTransmit(void) override;
+  void onAfterTransmit(void) override;
+  void enterDeepSleep(uint32_t secs, int pin_wake_btn = -1);
+  void powerOff() override;
+  uint16_t getBattMilliVolts() override;
+  const char* getManufacturerName() const override ;
+
+};
