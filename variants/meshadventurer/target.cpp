f2cff53b0eb57a267e5f519f0fb08e03bea86a3c
recrof <recrof@gmail.com>
2025-09-24 09:04:16 +0200

    fixed meshadventurer failing build

--- variants/meshadventurer/target.cpp
+++ variants/meshadventurer/target.cpp
@@ -16,6 +16,7 @@ MASensorManager sensors = MASensorManager(nmea);
 
 #ifdef DISPLAY_CLASS
   DISPLAY_CLASS display;
+  MomentaryButton user_btn(PIN_USER_BTN, 1000, true);
 #endif
 
 bool radio_init() {
